18f037b7b0939c8797f6c7befabe001a9d67d427
Abhishek <joshiabhishek026@gmail.com>
2024-07-24 03:11:24 +0530

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from "../data/genres"
 
 export interface Genres {
     id: number,
@@ -7,7 +7,7 @@ export interface Genres {
 
 }
 
-const useGenres = () => useData<Genres>('/genres')
+const useGenres = () => ({data: genres, isLoading: false})
 
 
 export default useGenres
